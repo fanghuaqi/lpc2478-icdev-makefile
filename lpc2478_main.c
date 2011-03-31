b1d7771ae4371cd4bc03e5e4e48f2e1888a986b5
hustfang890312@gmail.com <hustfang890312@gmail.com@4ce60ce5-bb70-eece-e08b-2053ad357e75>
2011-03-31 18:50:51 +0000

    LPC2478 ICDEV开发板 测试I2C写LED成功，出现的问题：STO写必须在清SI前。数据完毕后必须发送STO，否则就会出错

--- lpc2478_main.c
+++ lpc2478_main.c
@@ -13,20 +13,25 @@ const uint8_t* Hello = "Hello World!\n";
 
 void main(){
 
-	int temp ;
+	int temp,i,j ;
     PLL_Init();
     UART_Init(115200);
     I2C_Init(I2C_CHL0,I2CMASTER,I2C0_CLK,I2C0_ADDR);
     //I2C_Init(I2C_CHL1,I2CSLAVE,I2C1_CLK,I2C1_ADDR);
 
+    I2C_Master_WriteByte(I2C_CHL0, 0x60|0x0e, 0x01);
+    I2C_Master_WriteByte(I2C_CHL0, 0x60|0x08, 0x03);/*打开键盘，显示驱动*/
+    temp = I2C_Master_ReadByte(I2C_CHL0, 0x60|0x0); /*读芯片版本*/
+    UART_Printf("%d\n",temp);
+
     while(1){
+    	i = (i>3)?0:i;
+    	j = (j>255)?0:j;
     	UART_Printf(Hello);
     	Delay_ms(100);
-    	I2C_Master_WriteByte(I2C_CHL0, 0x60|0x0e, 0x01);
-    	//temp = I2C_Master_ReadByte(I2C_CHL0, 0x60|0x0); /*读芯片版本*/
-    	//UART_Printf("%d\n",temp);
-    	I2C_Master_WriteByte(I2C_CHL0, 0x60|0x08, 0x03);/*打开键盘，显示驱动*/
-    	I2C_Master_WriteByte(I2C_CHL0, 0x60|0x10, 0xff);
+    	I2C_Master_WriteByte(I2C_CHL0, 0x60|0x10|(i<<1), j);
+    	i++;
+    	j++;
     }
     
 }
