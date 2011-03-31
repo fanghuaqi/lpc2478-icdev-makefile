036d42f5a469a4d110ec2de93fe9805b523f0f6f
hustfang890312@gmail.com <hustfang890312@gmail.com@4ce60ce5-bb70-eece-e08b-2053ad357e75>
2011-03-31 18:17:48 +0000

    这个版本的程序已经可以与ch452进行I2C通讯了，ch452的地址是60H，但是和我设计的40h有差别，需要继续测试。 现在可以点亮led，但是写sto命令好像得不到答复，比较奇怪，仅仅出现在写led的时候。

--- trunk/lpc2478_main.c
+++ trunk/lpc2478_main.c
@@ -13,15 +13,20 @@ const uint8_t* Hello = "Hello World!\n";
 
 void main(){
 
+	int temp ;
     PLL_Init();
     UART_Init(115200);
     I2C_Init(I2C_CHL0,I2CMASTER,I2C0_CLK,I2C0_ADDR);
-    I2C_Init(I2C_CHL1,I2CSLAVE,I2C1_CLK,I2C1_ADDR);
+    //I2C_Init(I2C_CHL1,I2CSLAVE,I2C1_CLK,I2C1_ADDR);
 
     while(1){
     	UART_Printf(Hello);
     	Delay_ms(100);
-    	I2C_Master_WriteByte(I2C_CHL0, I2C1_ADDR, 0xff);
+    	I2C_Master_WriteByte(I2C_CHL0, 0x60|0x0e, 0x01);
+    	//temp = I2C_Master_ReadByte(I2C_CHL0, 0x60|0x0); /*读芯片版本*/
+    	//UART_Printf("%d\n",temp);
+    	I2C_Master_WriteByte(I2C_CHL0, 0x60|0x08, 0x03);/*打开键盘，显示驱动*/
+    	I2C_Master_WriteByte(I2C_CHL0, 0x60|0x10, 0xff);
     }
     
 }
