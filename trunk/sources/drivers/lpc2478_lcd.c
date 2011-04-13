e5fdf5c22d95b743568218f6412ac94a0f684ac9
hustfang890312@gmail.com <hustfang890312@gmail.com@4ce60ce5-bb70-eece-e08b-2053ad357e75>
2011-04-13 04:35:46 +0000

    LPC2478 ICDEV开发板 更新LM6800 LCD驱动的代码的核心驱动部分，初步编译通过未测试。

---
+++ trunk/sources/drivers/lpc2478_lcd.c
@@ -0,0 +1,147 @@
+/**
+ * @file   lpc2478_lcd.c
+ * @author fanghuaqi <578567190@qq.com>
+ * @date   Sun Apr 10 20:44:12 2011
+ * 
+ * @brief  lm6800 lcd driver for lpc2478
+ * 
+ * 
+ */
+
+#include "lpc2478_lcd.h"
+
+
+
+/** 
+ * 
+ * 
+ * 
+ * @return 
+ */
+ERCD LCD_Init(void)
+{
+    uint8_t i ;
+    LCD_RST_LOW();                  /*RST must keep low at least 1us then go high*/
+    DELAY_US();
+    LCD_RST_HIGH();
+    DELAY_US();
+    for (i = 0; i < 4; i++){
+        LCD_Chip_Select(i);
+        LCD_Write_CMD(LCD_DISPLAY_ON);/*Display on*/
+        LCD_Write_CMD(LCD_DISPLAY_START_ADDR|0x0);/*display startline register 0*/
+    }
+    return ERCD_OK;
+}
+/** 
+ * 
+ * 
+ * @param cmdcode 
+ * 
+ * @return 
+ */
+ERCD LCD_Write_CMD(uint8_t cmdcode)
+{
+    LCD_RS_LOW();                   /*when RS=0 R/W=0,cmd write the data bus*/
+    LCD_R_W_LOW();
+    LCD_OUT_DATA(cmdcode);          /*output cmd  on the bus*/
+    LCD_E_HIGH_LOW();               /*when E high->low data appear on the bus*/
+    return ERCD_OK;
+}
+/** 
+ * 
+ * 
+ * @param char_data 
+ * 
+ * @return 
+ */
+ERCD LCD_Write_Char(uint8_t char_data)
+{
+    LCD_RS_HIGH();                  /*when RS=1 R/W=0,cmd write the data bus*/
+    LCD_R_W_LOW();
+    LCD_OUT_DATA(char_data);        /*output data on the bus*/
+    LCD_E_HIGH_LOW();               /*when E high->low(falling edge) data appear on the bus*/
+    return ERCD_OK;
+}
+/** 
+ * 
+ * 
+ * 
+ * @return 
+ */
+uint8_t LCD_Read_Char(void)
+{
+    uint8_t data_read;
+    /*when read data ,a dummy read is needed*/
+    LCD_RS_HIGH();                   /*when RS=1 R/W=1,read data from the displayram*/
+    LCD_R_W_HIGH();
+    data_read = LCD_IN_DATA();       /*in data on the bus*/
+    LCD_E_HIGH();                    /*when E=1 and device is selected data appear*/
+    return data_read;
+}
+/** 
+ * 
+ * 
+ * 
+ * @return 
+ */
+uint8_t LCD_Read_Status(void)
+{
+    uint8_t status;
+    /*when read status ,a dummy read is not needed*/
+    LCD_RS_LOW();                  /*when RS=0 R/W=1,read status ofthe l*/
+    LCD_R_W_HIGH();
+    status= LCD_IN_DATA();         /*n data on the bus*/
+    LCD_E_HIGH();                   /*when E=1 and device is selecte dat ppear*/
+    return status;
+}
+/** 
+  
+  * 
+  * param cipsel 
+  * 
+  * @return 
+  */
+ERCD LCD_Chip_Select(uint8_t chipsel)
+{
+    if (chipsel > 3){
+        return ERCD_ARG_ERR;
+    }
+    switch (chipsel){
+        case 0:
+            LCD_CHIP_SEL_0();
+            break;
+        case 1:
+            LCD_CHIP_SEL_1();
+            break;
+        case 2:
+            LCD_CHIP_SEL_2();
+            break;
+        case 3:
+            LCD_CHIP_SEL_3();
+            break;
+        default:
+            break;
+    }
+    return ERCD_OK;
+}
+/* 
+* 
+* 
+* @ara row 
+ * @param col 
+ * 
+ * @return 
+ */
+ERCD LCD_Locate(uint16_t row, uint16_t col)
+{
+    uint8_t chipsel = 0;
+    if ( (col < LCD_COLS) && (row < LCD_ROWS) ){ /*check if the row and col is over range */
+        chipsel = col / LCD_CHIP_WIDTH;
+        LCD_Chip_Select(chipsel);
+    } else {
+        return ERCD_ARG_ERR;
+    }
+    LCD_Write_Char(LCD_SET_X_ADDRESS|(row>>3)); /*Page address*/
+    LCD_Write_Char(LCD_SET_Y_ADDRESS|(col%LCD_CHIP_WIDTH));
+    return ERCD_OK;
+}
---
+++ trunk/sources/includes/lpc2478_lcd.h
@@ -0,0 +1,63 @@
+/**
+ * @file   lpc2478_lcd.h
+ * @author fanghuaqi <578567190@qq.com>
+ * @date   Sun Apr 10 20:42:56 2011
+ *
+ * @brief  the lm6800 lcd driver
+ *
+ *
+ */
+
+#ifndef  LPC2478_LCD_H
+#define  LPC2478_LCD_H
+
+
+
+#include  "lpc2478_inc.h"
+
+/*pin assignment*/
+#define LCD_RST PINSEL4
+#define LCD_RS PINSEL4
+#define LCD_R_W PINSEL4
+#define LCD_E PINSEL4
+#define LCD_CSA PINSEL4
+#define LCD_CSB PINSEL4
+#define LCD_CSC PINSEL4
+#define LCD_DATA_BUS PINSEL4
+
+#define LCD_DISPLAY_ON  1
+#define LCD_DISPLAY_START_ADDR 1
+#define LCD_SET_X_ADDRESS 1
+#define LCD_SET_Y_ADDRESS 1
+#define LCD_CHIP_WIDTH 1
+#define LCD_ROWS 1
+#define LCD_COLS 1
+/*marco operation instruction*/
+#define LCD_RST_LOW()     LCD_RST = 0 /*RST:Reset signal high*/
+#define LCD_RST_HIGH()    LCD_RST = 1 /*reset signal low*/
+#define LCD_RS_HIGH()     LCD_RS = 1  /*RS:data or instruction select signal high*/
+#define LCD_RS_LOW()      LCD_RS = 0  /*RS:data or instruction select signal low*/
+#define LCD_R_W_HIGH()    LCD_R_W = 1 /*read or write signal high*/
+#define LCD_R_W_LOW()     LCD_R_W = 0 /*read or write signal low*/
+#define LCD_E_HIGH()      LCD_E = 1   /*Enable signal high*/
+#define LCD_E_LOW()       LCD_E = 0   /*Enable signal low*/
+#define LCD_E_HIGH_LOW()  {LCD_E_HIGH();LCD_E_LOW();} /*E falling edge*/
+#define LCD_IN_DATA()     LCD_DATA_BUS /*the lcd data bus*/
+#define LCD_OUT_DATA(c)   LCD_DATA_BUS = (c) /*output data on the data bus*/
+#define DELAY_US()    {}                   /*delay 1 us*/
+#define LCD_CHIP_SEL_0()   {LCD_CSA = 0;LCD_CSB = 0;LCD_CSC=0;} /*select the left-most section*/
+#define LCD_CHIP_SEL_1()   {LCD_CSA = 0;LCD_CSB = 0;LCD_CSC=1;} /*select the left-most section*/
+#define LCD_CHIP_SEL_2()   {LCD_CSA = 0;LCD_CSB = 1;LCD_CSC=0;} /*select the left-most section*/
+#define LCD_CHIP_SEL_3()   {LCD_CSA = 0;LCD_CSB = 1;LCD_CSC=1;} /*select the left-most section*/
+#define LCD_CHIP_DISABLE_ALL() {LCD_CSA = 1}           /*Disable all the access to the LCD module*/
+
+/*function declaration*/
+ERCD LCD_Init(void);
+ERCD LCD_Write_CMD(uint8_t cmdcode);
+ERCD LCD_Write_Char(uint8_t char_data);
+uint8_t LCD_Read_Char(void);
+uint8_t LCD_Read_Status(void);
+ERCD LCD_Chip_Select(uint8_t chipsel);
+ERCD LCD_Locate(uint16_t row, uint16_t col);
+
+#endif
