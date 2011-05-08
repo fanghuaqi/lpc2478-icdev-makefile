d69ae712e9fe3fc993d3e414f5c9d375b9419bf0
hustfang890312@gmail.com <hustfang890312@gmail.com@4ce60ce5-bb70-eece-e08b-2053ad357e75>
2011-05-08 09:00:22 +0000

    现在能够正常的读写胡航笛的LPC2478的板子上的eeprom，但是必须在写数据的时候加上     I2C_SEND(i2c_baseAddr,data);  //写数据     //Delay_ms(5);     I2C_CLEAR_STAT(i2c_baseAddr,I2CONCLR_SIC|I2CONCLR_AAC); //等待数据写入完毕     I2C_ACK(i2c_baseAddr,0x28);                             //等待数据写入完毕     I2C_STOP(i2c_baseAddr);                                 //等待数据写入完毕
    还有每写一次数据需要延时4ms以上才能写下一个字节的数据，否则就写入失败

--- trunk/sources/drivers/lpc2478_i2c.c
+++ trunk/sources/drivers/lpc2478_i2c.c
@@ -483,14 +483,14 @@ ERCD EEPROM_WriteByte(uint8_t i2c_channel, uint32_t addr, uint8_t data)
     I2C_CLEAR_STAT(i2c_baseAddr,I2CONCLR_SIC|I2CONCLR_AAC);
     I2C_ACK(i2c_baseAddr,0x28);
     I2C_SEND(i2c_baseAddr,data);
-    //Delay_ms(10);
-    //I2C_CLEAR_STAT(i2c_baseAddr,I2CONCLR_SIC);
-    //I2C_ACK(i2c_baseAddr,0x28);
+    //Delay_ms(5);
+    I2C_CLEAR_STAT(i2c_baseAddr,I2CONCLR_SIC|I2CONCLR_AAC);
+    I2C_ACK(i2c_baseAddr,0x28);
     I2C_STOP(i2c_baseAddr);
     return ERCD_OK;
     //I2C_Master_WriteByte(i2c_channel, eeprom_addr,(uint8_t)(addr>>8)); /*High addr Half first*/
     //I2C_Master_WriteByte(i2c_channel, eeprom_addr,(uint8_t)addr); /*low addr Half follow*/
-    //return I2C_Master_WriteByte(i2c_channel, eeprom_addr,data);
+    return I2C_Master_WriteByte(i2c_channel, eeprom_addr,data);
 }
 
 /** 
