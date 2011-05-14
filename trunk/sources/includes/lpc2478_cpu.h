4c02b9c83281ce1343f8d6c4e36915028c1d6a71
hustfang890312@gmail.com <hustfang890312@gmail.com@4ce60ce5-bb70-eece-e08b-2053ad357e75>
2011-05-14 03:40:29 +0000

    添加sil.h中的调用但是暂时未用上

--- trunk/sources/includes/lpc2478_cpu.h
+++ trunk/sources/includes/lpc2478_cpu.h
@@ -24,7 +24,7 @@ typedef  signed   int    int16_t;
 typedef  unsigned char   uint8_t;
 typedef  signed   char   int8_t;
 typedef  unsigned char   ERCD;
-
+#define Inline	static __inline__	/* インライン関数 */
 #define  setreg(regaddr,value)  		 {(*(volatile unsigned long *)(regaddr)) = (value);}
 
 #define  setregbits(reg,mask,value) 	 {(reg) = ((reg)&(mask))|(value);}
@@ -33,4 +33,19 @@ typedef  unsigned char   ERCD;
 #define  setregbit(regaddr,mask,value)   {getreg(regaddr) = ((getreg(regaddr))&(mask))|(value);}
 #define  MASK_ALL						 0xffffffff
 
+
+Inline uint32_t
+sil_rew_mem(void *mem)
+{
+	uint32_t	data;
+
+	data = *((volatile uint32_t *) mem);
+	return(data);
+}
+
+Inline void
+sil_wrw_mem(void *mem, uint32_t data)
+{
+	*((volatile uint32_t *) mem) = data;
+}
 #endif
