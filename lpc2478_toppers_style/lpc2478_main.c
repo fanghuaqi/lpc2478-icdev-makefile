de8ae3d8724c8f7e5dea0f2255636b8e8bfd1341
hustfang890312@gmail.com <hustfang890312@gmail.com@4ce60ce5-bb70-eece-e08b-2053ad357e75>
2011-05-19 11:56:12 +0000

    除了IIC,LCD的代码没有转移到TOPPERS风格下其余均已转至 更改到TOPPERS环境下后 测试了如下几个模块:

--- lpc2478_toppers_style/lpc2478_main.c
+++ lpc2478_toppers_style/lpc2478_main.c
@@ -10,17 +10,48 @@
 #include "lpc2478_inc.h"
 
 volatile uint8_t gl_pCH452_key = 0;
-int main(){
+int main()
+{
+	RTCTime local_time, alarm_time, current_time;
 
-    PLL_Init();
+    /*pll init*/
+	PLL_Init();
     Buzzer_Init();
+    UART_Init(115200);
+    RTC_Init();
+    pwm_init(PWM_CHANNEL_1,1000);
     timer2_capinit();
 
+	local_time.RTC_Sec = 00;
+	local_time.RTC_Min = 51;
+	local_time.RTC_Hour = 19;
+	local_time.RTC_Mday = 19;
+	local_time.RTC_Wday = 4;
+	local_time.RTC_Yday = 139;		/* current date 05/10/2011 */
+	local_time.RTC_Mon = 5;
+	local_time.RTC_Year = 2011;
+	//RTC_SetTime( local_time );		/* Set local time */
+	alarm_time.RTC_Sec = 0;
+	alarm_time.RTC_Min = 52;
+	alarm_time.RTC_Hour = 19;
+	alarm_time.RTC_Mday = 19;
+	alarm_time.RTC_Wday = 4;
+	alarm_time.RTC_Yday = 139;		/* alarm date 05/10/2011 */
+	alarm_time.RTC_Mon = 5;
+	alarm_time.RTC_Year = 2011;
+	RTC_SetAlarm(alarm_time);
+	RTC_Start();
     while(1){
-    	Buzzer(BUZZER_ON);
-    	Delay_ms(1);
-    	Buzzer(BUZZER_OFF);
-    	Delay_ms(1);
+    	//pwm_setpwm1duty(PWM_PORT_2,20);
+    	//pwm_setpwm1duty(PWM_PORT_1,20);
+    	//Buzzer(BUZZER_ON);
+    	//timer2_delayms(1);
+    	//Buzzer(BUZZER_OFF);
+    	timer2_delayms(1000);
+    	current_time = RTC_GetTime();
+    	UART_Printf("Time: %d-%d-%d\t%d:%d:%d\n",current_time.RTC_Year,current_time.RTC_Mon,current_time.RTC_Mday,current_time.RTC_Hour,current_time.RTC_Min,current_time.RTC_Sec);
+
+    	PRINT_Log("Hello World!\n");
     }
     return 0;
 }
